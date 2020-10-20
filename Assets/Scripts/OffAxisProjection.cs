166793c51fcf9938d498e3f4b219484a7e079258
leafyjones <57333171+leafyjones@users.noreply.github.com>
2020-10-20 13:42:18 -0400

    Initial commit

---
+++ Assets/Scripts/OffAxisProjection.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffAxisProjection : MonoBehaviour {
+    public Camera myCamera;
+
+    //public LineRenderer lineRenderer;
+
+    /*
+        normalize euler? move from there, depending on the normalize?
+        will that avoid gimabls? 
+
+        get scale factor correct
+        try some shit to calculate distance correctly
+        1. set portalpos as -1 or 0. Gotta find what my portal pos is. Or make my own "portal".
+        2. work more similiarly to eye tracking example
+
+        correctly set up left, right, top bottom. In meters? Or do like 1/2 like in other exmaple. 
+
+        devicecam is their portal cam? I think
+    */
+
+    public float left, right, bottom, top, near, far;
+
+    public float nearDist;
+
+    float distance;
+
+    Quaternion orientation;
+
+    public GameObject RotationSphere;
+
+    Quaternion offset;
+
+    //Every frame try to get gyroscope commands, until they kick in. 
+    IEnumerator InitializeGyro () {
+        Input.gyro.enabled = true;
+        offset = Input.gyro.attitude;
+        while (offset == Quaternion.identity) {
+            offset = Input.gyro.attitude;
+            yield return null;
+        }
+        Debug.Log ("Awake" + offset);
+    }
+
+    void Awake () {
+        StartCoroutine (InitializeGyro ());
+    }
+
+    void Start () {
+        //RotationSphere.transform.rotation = Random.rotation;
+        //offset = Input.gyro.attitude;
+        //Debug.Log ("START " + offset.eulerAngles.x + " " + offset.eulerAngles.y + " " + offset.eulerAngles.z);
+        //difference between this and identity
+    }
+
+    void LateUpdate () {
+
+        orientation = Quaternion.Inverse (offset) * Input.gyro.attitude;
+        //Vector3 myCamPos = myCamera.ScreenToWorldPoint ();
+        //orientation = Quaternion.Inverse (offset) * RotationSphere.transform.rotation;
+
+        // landscape iPhone X, measures in meters
+        left = myCamera.transform.position.x - 0.000f;
+        right = myCamera.transform.position.x + 0.135f;
+        top = myCamera.transform.position.y + 0.022f;
+        bottom = myCamera.transform.position.y - 0.040f;
+
+        distance = Mathf.Abs (myCamera.transform.position.z);
+
+        far = 10f; // may need bigger for bigger scenes, max 10 metres for now
+        near = 0.1f;
+
+        Vector3 topLeft = new Vector3 (left, top, near);
+        Vector3 topRight = new Vector3 (right, top, near);
+        Vector3 bottomLeft = new Vector3 (left, bottom, near);
+        Vector3 bottomRight = new Vector3 (right, bottom, near);
+
+        // move near to 0.01 (1 cm from eye)
+        float scale_factor = near / distance; //0.01f / near; near was one
+        near *= scale_factor;
+        left *= scale_factor;
+        right *= scale_factor;
+        top *= scale_factor;
+        bottom *= scale_factor;
+
+        Matrix4x4 m = PerspectiveOffCenter (left, right, bottom, top, near, far);
+        myCamera.projectionMatrix = m;
+
+        //Vector3 euler = EulerClamp (orientation);
+        //1 - 0.5f * Mathf.Min (Mathf.Abs (myCamera.transform.position.x) + Mathf.Abs (myCamera.transform.position.y), 1)
+
+        /*
+        myCamera.transform.position = new Vector3 (
+            (-euler.y / 60),
+            euler.x / 60, -1f
+        );
+        */
+        Vector3 directionFromQ = orientation * Vector3.forward;
+        myCamera.transform.position = directionFromQ;
+        myCamera.transform.position = new Vector3 (myCamera.transform.position.x, myCamera.transform.position.y, -1f);
+        //Debug.Log ("UPDATE " + directionFromQ.x);
+    }
+
+    private Vector3 EulerClamp (Quaternion orientation) {
+        Vector3 euler = orientation.eulerAngles;
+
+        if (euler.y >= 180f) {
+            euler.y -= 360;
+            euler.y = Mathf.Abs (euler.y);
+        }
+        if (euler.x >= 180f) {
+            euler.x -= 360;
+            euler.x = Mathf.Abs (euler.x);
+        }
+        if (euler.z >= 180f) {
+            euler.z -= 360;
+            euler.z = Mathf.Abs (euler.z);
+        }
+
+        return euler;
+    }
+
+    private static Quaternion GyroToUnity (Quaternion q) {
+        return new Quaternion (q.x, q.y, -q.z, -q.w);
+    }
+
+    static Matrix4x4 PerspectiveOffCenter (float left, float right, float bottom, float top, float near, float far) {
+        float x = 2.0F * near / (right - left);
+        float y = 2.0F * near / (top - bottom);
+        float a = (right + left) / (right - left);
+        float b = (top + bottom) / (top - bottom);
+        float c = -(far + near) / (far - near);
+        float d = -(2.0F * far * near) / (far - near);
+        float e = -1.0F;
+        Matrix4x4 m = new Matrix4x4 ();
+        m[0, 0] = x;
+        m[0, 1] = 0;
+        m[0, 2] = a;
+        m[0, 3] = 0;
+        m[1, 0] = 0;
+        m[1, 1] = y;
+        m[1, 2] = b;
+        m[1, 3] = 0;
+        m[2, 0] = 0;
+        m[2, 1] = 0;
+        m[2, 2] = c;
+        m[2, 3] = d;
+        m[3, 0] = 0;
+        m[3, 1] = 0;
+        m[3, 2] = e;
+        m[3, 3] = 0;
+        return m;
+    }
+}
---
+++ OffAxisIos/Classes/Native/UnityEngine.InputLegacyModule_CodeGen.c
@@ -0,0 +1,134 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void UnityEngine.Gyroscope::.ctor(System.Int32)
+extern void Gyroscope__ctor_m59C98DED96D3AC3EC33D22242AB68CBF05AA3540 (void);
+// 0x00000002 UnityEngine.Quaternion UnityEngine.Gyroscope::attitude_Internal(System.Int32)
+extern void Gyroscope_attitude_Internal_mFD7EC5584645E9F3EAF099D96837D449B86229E5 (void);
+// 0x00000003 System.Void UnityEngine.Gyroscope::setEnabled_Internal(System.Int32,System.Boolean)
+extern void Gyroscope_setEnabled_Internal_mA8233E80210D91EBB36F768E5F5B3A7F9CA48BFB (void);
+// 0x00000004 UnityEngine.Quaternion UnityEngine.Gyroscope::get_attitude()
+extern void Gyroscope_get_attitude_m599BF954B806CD2CA5A1C7C97A32E6B8BE6E6481 (void);
+// 0x00000005 System.Void UnityEngine.Gyroscope::set_enabled(System.Boolean)
+extern void Gyroscope_set_enabled_mCB6436A1155EE9C7D81B96F58F15E75FCE95C1D1 (void);
+// 0x00000006 System.Void UnityEngine.Gyroscope::attitude_Internal_Injected(System.Int32,UnityEngine.Quaternion&)
+extern void Gyroscope_attitude_Internal_Injected_m8717A035C3FDA9FD7B55234BE57E839E602CFBBE (void);
+// 0x00000007 UnityEngine.GameObject UnityEngine.CameraRaycastHelper::RaycastTry(UnityEngine.Camera,UnityEngine.Ray,System.Single,System.Int32)
+extern void CameraRaycastHelper_RaycastTry_m8AA2714ED46E79851C77B83A3916C515D7280FD1 (void);
+// 0x00000008 UnityEngine.GameObject UnityEngine.CameraRaycastHelper::RaycastTry2D(UnityEngine.Camera,UnityEngine.Ray,System.Single,System.Int32)
+extern void CameraRaycastHelper_RaycastTry2D_mAA0B0BAC7BE8A2F640A236BB6655EB47E5408C9D (void);
+// 0x00000009 UnityEngine.GameObject UnityEngine.CameraRaycastHelper::RaycastTry_Injected(UnityEngine.Camera,UnityEngine.Ray&,System.Single,System.Int32)
+extern void CameraRaycastHelper_RaycastTry_Injected_mFAED8F3DC204691A92849F36D8BC69CFE68F43E8 (void);
+// 0x0000000A UnityEngine.GameObject UnityEngine.CameraRaycastHelper::RaycastTry2D_Injected(UnityEngine.Camera,UnityEngine.Ray&,System.Single,System.Int32)
+extern void CameraRaycastHelper_RaycastTry2D_Injected_m1AB3B5B899C17D5D6FC41D8F8CF2BE549F7D9F21 (void);
+// 0x0000000B System.Boolean UnityEngine.Input::GetMouseButton(System.Int32)
+extern void Input_GetMouseButton_m27BF2DDBF38A38787B83A13D3E6F0F88F7C834C1 (void);
+// 0x0000000C System.Boolean UnityEngine.Input::GetMouseButtonDown(System.Int32)
+extern void Input_GetMouseButtonDown_m466D81FDCC87C9CB07557B39DCB435EB691F1EF3 (void);
+// 0x0000000D UnityEngine.Vector3 UnityEngine.Input::get_mousePosition()
+extern void Input_get_mousePosition_m79528BC2F30C57054641F709C855130AE586AC0E (void);
+// 0x0000000E System.Int32 UnityEngine.Input::GetGyroInternal()
+extern void Input_GetGyroInternal_m7DE349896B9EEFE14A16A6D568ED3DA341C887CB (void);
+// 0x0000000F UnityEngine.Gyroscope UnityEngine.Input::get_gyro()
+extern void Input_get_gyro_mCDF4A8D1C7F0A599A13B94376AB49822AAB32FBF (void);
+// 0x00000010 System.Void UnityEngine.Input::get_mousePosition_Injected(UnityEngine.Vector3&)
+extern void Input_get_mousePosition_Injected_m4E5460D301ECE27CC322AB79C0914A4503ABC06E (void);
+// 0x00000011 System.Void UnityEngine.SendMouseEvents::SetMouseMoved()
+extern void SendMouseEvents_SetMouseMoved_mEC659144183FB490A2E1F12112C8F08569A511CD (void);
+// 0x00000012 System.Void UnityEngine.SendMouseEvents::DoSendMouseEvents(System.Int32)
+extern void SendMouseEvents_DoSendMouseEvents_m21561D473C27F19BA9CDBC53B4A13D40DDFBE785 (void);
+// 0x00000013 System.Void UnityEngine.SendMouseEvents::SendEvents(System.Int32,UnityEngine.SendMouseEvents_HitInfo)
+extern void SendMouseEvents_SendEvents_m7A59BBFBB15C1BF3E47D56CDD4921599A686F9C7 (void);
+// 0x00000014 System.Void UnityEngine.SendMouseEvents::.cctor()
+extern void SendMouseEvents__cctor_m6B63654E024F338414361C995EAFEC615743A0E0 (void);
+// 0x00000015 System.Void UnityEngine.SendMouseEvents_HitInfo::SendMessage(System.String)
+extern void HitInfo_SendMessage_m2D813691948EAB9CDA487A3B8668678EABFCFA62_AdjustorThunk (void);
+// 0x00000016 System.Boolean UnityEngine.SendMouseEvents_HitInfo::op_Implicit(UnityEngine.SendMouseEvents_HitInfo)
+extern void HitInfo_op_Implicit_m8332A3930623A2248D797F3A8020FFF4E05A9420 (void);
+// 0x00000017 System.Boolean UnityEngine.SendMouseEvents_HitInfo::Compare(UnityEngine.SendMouseEvents_HitInfo,UnityEngine.SendMouseEvents_HitInfo)
+extern void HitInfo_Compare_m3AD170E7A52826C73DEF8C3C8F6507C9EAB28363 (void);
+static Il2CppMethodPointer s_methodPointers[23] = 
+{
+	Gyroscope__ctor_m59C98DED96D3AC3EC33D22242AB68CBF05AA3540,
+	Gyroscope_attitude_Internal_mFD7EC5584645E9F3EAF099D96837D449B86229E5,
+	Gyroscope_setEnabled_Internal_mA8233E80210D91EBB36F768E5F5B3A7F9CA48BFB,
+	Gyroscope_get_attitude_m599BF954B806CD2CA5A1C7C97A32E6B8BE6E6481,
+	Gyroscope_set_enabled_mCB6436A1155EE9C7D81B96F58F15E75FCE95C1D1,
+	Gyroscope_attitude_Internal_Injected_m8717A035C3FDA9FD7B55234BE57E839E602CFBBE,
+	CameraRaycastHelper_RaycastTry_m8AA2714ED46E79851C77B83A3916C515D7280FD1,
+	CameraRaycastHelper_RaycastTry2D_mAA0B0BAC7BE8A2F640A236BB6655EB47E5408C9D,
+	CameraRaycastHelper_RaycastTry_Injected_mFAED8F3DC204691A92849F36D8BC69CFE68F43E8,
+	CameraRaycastHelper_RaycastTry2D_Injected_m1AB3B5B899C17D5D6FC41D8F8CF2BE549F7D9F21,
+	Input_GetMouseButton_m27BF2DDBF38A38787B83A13D3E6F0F88F7C834C1,
+	Input_GetMouseButtonDown_m466D81FDCC87C9CB07557B39DCB435EB691F1EF3,
+	Input_get_mousePosition_m79528BC2F30C57054641F709C855130AE586AC0E,
+	Input_GetGyroInternal_m7DE349896B9EEFE14A16A6D568ED3DA341C887CB,
+	Input_get_gyro_mCDF4A8D1C7F0A599A13B94376AB49822AAB32FBF,
+	Input_get_mousePosition_Injected_m4E5460D301ECE27CC322AB79C0914A4503ABC06E,
+	SendMouseEvents_SetMouseMoved_mEC659144183FB490A2E1F12112C8F08569A511CD,
+	SendMouseEvents_DoSendMouseEvents_m21561D473C27F19BA9CDBC53B4A13D40DDFBE785,
+	SendMouseEvents_SendEvents_m7A59BBFBB15C1BF3E47D56CDD4921599A686F9C7,
+	SendMouseEvents__cctor_m6B63654E024F338414361C995EAFEC615743A0E0,
+	HitInfo_SendMessage_m2D813691948EAB9CDA487A3B8668678EABFCFA62_AdjustorThunk,
+	HitInfo_op_Implicit_m8332A3930623A2248D797F3A8020FFF4E05A9420,
+	HitInfo_Compare_m3AD170E7A52826C73DEF8C3C8F6507C9EAB28363,
+};
+static const int32_t s_InvokerIndices[23] = 
+{
+	32,
+	1100,
+	1101,
+	1026,
+	31,
+	1102,
+	1103,
+	1103,
+	1104,
+	1104,
+	46,
+	46,
+	1001,
+	508,
+	4,
+	17,
+	3,
+	121,
+	1105,
+	3,
+	26,
+	1106,
+	1107,
+};
+extern const Il2CppCodeGenModule g_UnityEngine_InputLegacyModuleCodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_InputLegacyModuleCodeGenModule = 
+{
+	"UnityEngine.InputLegacyModule.dll",
+	23,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
